c5678f72e49e24243afc876f6638e45005e34d08
LuboFrancisco <lupanchox@gmail.com>
2022-07-15 20:37:36 -0300

    styles landing page

--- client/src/Components/Landing.tsx
+++ client/src/Components/Landing.tsx
@@ -1,13 +1,18 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
 import flora from '../images/Flora-Showroom.jpg'
+import { LandingBackground, LandingDiv, SecondDiv } from './Landing.module'
 
 const Landing = () => {
     return (
-        <div>
-            <img src={flora} alt="imagen de fondo" />
-            <h1>Flora Showroom</h1>
-            <p>...</p>
-        </div>
+        <LandingDiv>
+            <LandingBackground>
+                <img src={flora} alt="imagen de fondo" />
+            </LandingBackground>
+            <SecondDiv>
+                <h1>Flora Showroom</h1>
+                <Link to='/home'>¡Ingresar!</Link>
+            </SecondDiv>
+        </LandingDiv>
     )
 }
 
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,17 +3,21 @@ import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import store from './redux/store'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import App from './App'
 import Form from './Components/Form'
 import Home from './Components/Home'
 import Landing from './Components/Landing'
+import Navbar from './Components/Navbar'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Landing />}/>
+          {
+            location.href === 'http://localhost:3000/'
+            ? <Route path='/' element={<Landing />}/>
+            : <Route path='/' element={<Navbar />}/>
+          }
           <Route path='/home' element={<Home />}/>
           <Route path='/form/:id' element={<Form />}/>
           </Routes>

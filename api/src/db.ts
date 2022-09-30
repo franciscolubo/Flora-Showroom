f2068b9b1d57491ad69352c683e17c383ecdcccf
LuboFrancisco <lupanchox@gmail.com>
2022-09-30 00:12:36 -0300

    correction ignore

--- api/src/db.ts
+++ api/src/db.ts
@@ -1,9 +1,7 @@
-import mongoose, { ConnectOptions } from 'mongoose'
+import mongoose from 'mongoose'
 
 const connectDatabase = async () => {
-  await mongoose.connect(process.env.MONGO_URI as string, {
-    useNewUrlParser: true
-  } as ConnectOptions)
+  await mongoose.connect(process.env.MONGO_URI as string)
     .then(db => { console.log('MongoDB Servidor conectado', db.connection.host) })
     .catch(err => { console.log('Error en la conexion de la database', err) })
 }
